5ffac82676695c2944ea04d30ea3b7d4e8070a71
JayeshJavaWarrior <jayeshlondhe01@gmail.com>
2024-06-26 15:18:20 +0530

    Add files via upload

---
+++ src/main/java/com/pdf_to_images/PdfToImagesApplication.java
@@ -0,0 +1,13 @@
+package com.pdf_to_images;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdfToImagesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PdfToImagesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pdf_to_images/PdfToImagesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pdf_to_images;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PdfToImagesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
